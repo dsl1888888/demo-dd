67db7ad7b79b7c24248b98a779a6fb471a667e35
dorosdebby@gmail.com <dorosdebby@gmail.com>
2020-11-23 21:33:42 +0800

    add simple zuul+service

--- demo-cloud-service02/src/main/java/com/example/demo/DemoCloudService02Application.java
+++ demo-cloud-service02/src/main/java/com/example/demo/DemoCloudService02Application.java
@@ -2,8 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@org.springframework.cloud.client.discovery.EnableDiscoveryClient
+@EnableFeignClients
+@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class DemoCloudService02Application {
 
