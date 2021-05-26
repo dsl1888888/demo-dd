be46e938c75f99f6cdc89785cbf06910fe9d2e9b
MSI\dev01 <dorosdebby@gmail.com>
2021-05-26 14:52:26 +0800

    add demo-simple-use-springboot-admin-

---
+++ demo-simple-use-springboot-admin-client/src/main/java/com/example/demo/DemoSimpleUseSpringbootAdminClientApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+ 
+
+/**
+ * https://blog.csdn.net/u012702547/article/details/88775298
+ */
+@SpringBootApplication
+public class DemoSimpleUseSpringbootAdminClientApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DemoSimpleUseSpringbootAdminClientApplication.class, args);
+    }
+
+}
