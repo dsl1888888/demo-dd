0fbe655d6cc5d40f507898de046fb3f2becc3140
MSI\dev01 <dorosdebby@gmail.com>
2021-06-09 17:11:33 +0800

    add  demo springboot admin

---
+++ demo-springboot-admin-parent/demo-springboot-admin-client/src/main/java/com/example/demo/DemoSpringbootAdminClientApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+ 
+
+/**
+ * https://blog.csdn.net/u012702547/article/details/88775298
+ */
+@SpringBootApplication
+public class DemoSpringbootAdminClientApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DemoSpringbootAdminClientApplication.class, args);
+    }
+
+}
---
+++ demo-springboot-admin-parent/demo-springboot-admin-server/src/main/java/com/example/demo/DemoSpringbootAdminServerApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@EnableAdminServer
+@SpringBootApplication
+public class DemoSpringbootAdminServerApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DemoSpringbootAdminServerApplication.class, args);
+    }
+
+}
